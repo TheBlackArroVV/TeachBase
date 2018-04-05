45efed0bc923e1fafd98ab149727b088c4e314e2
TheBlackArroVV <theablackarrovv@gmail.com>
2018-04-05 10:46:27 +0300

    trains

--- main.rb
+++ main.rb
@@ -6,15 +6,18 @@ require_relative 'passenger_carriage.rb'
 require_relative 'cargo_carriage.rb'
 require_relative 'station.rb'
 
-stations = []
-trains = []
-carriages = []
+stations = {}
+trains = {}
 
 loop do
   puts "Enter 1 to create station"
   puts "Enter 2 to create train"
   puts "Enter 3 to create passenger train"
   puts "Enter 4 to create cargo train"
+  puts "Enter 5 to add carriage to train"
+  puts "Enter 6 to delete carriage from train"
+  puts "Enter 7 to send train to station"
+  puts "Enter 8 to see trains"
   puts "Enter 9 to see stations"
   puts "Enter 0 to exit"
   input = gets.chomp.to_i
@@ -23,22 +26,48 @@ loop do
     break
   when 1
     puts "Enter name of station"
-    stations << Station.new(gets.chomp)
+    name = gets.chomp
+    stations[name] = Station.new(name)
   when 2
+    puts "Enter name of trains"
     name = gets.chomp
+    puts "Enter type of train"
     type = gets.chomp
-    trains << Train.new(name, type)
+    trains[name] = Train.new(name, type)
   when 3
+    puts "Enter name of train"
     name = gets.chomp
-    trains << PassengerTrain.new(name)
+    trains[name] = PassengerTrain.new(name)
   when 4
+    puts "Enter name of train"
     name = gets.chomp
-    trains << CargoTrain.new(name)
-  when 9
+    trains[name] = CargoTrain.new(name)
+  when 5
+    puts "Enter name of train"
+    name = gets.chomp
+    trains[name].carriages << Carriage.new(trains[name].type)
+  when 6
+    puts "Enter name of train"
+    name = gets.chomp
+    trains[name].carriages.shift
+  when 7
+    puts "Enter name of station"
+    station_name = gets.chomp
+    puts "Enter name of train"
+    train_name = gets.chomp
+    stations[station_name].get_train(trains[train_name])
+  when 8
     puts ""
-    stations.each do |station|
-      puts station.name
+    trains.each do |name, train|
+      puts "#{name} has #{train.carriages.count} carriages"
     end
     puts ""
+  when 9
+    stations.each do |name, station|
+      puts "On station #{name}:"
+      station.trains.each do |train|
+        puts train.number
+      end
+    end
   end
 end
